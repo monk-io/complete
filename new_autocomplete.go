4836fc3d47560c82d047898a44104ca249700bb0
Vlad K <vladkrapivin1995@gmail.com>
2022-12-23 23:11:39 +0200

    [bug] improved autocomplete: added missing usecases, fixed dynamic args and flag values suggestions;refactor

--- new_autocomplete.go
+++ new_autocomplete.go
@@ -10,147 +10,245 @@ import (
 type CompTree struct {
 	Flags map[string]*CompTree
 	// FIXME: Args & Sub may be able to be combined since thus far there doesn't seem to necessitate a difference
-	Args    map[string]*CompTree
-	Sub     map[string]*CompTree
-	Dynamic func(prefix string) []Suggestion
-	Desc    string
-	Name    string
+	Args       map[string]*CompTree
+	Sub        map[string]*CompTree
+	Dynamic    func(prefix string) []Suggestion
+	Desc       string
+	Name       string
+	TakesValue bool
 }
 
 type SearchMethod func(s, query string) bool
+
+var (
+	SuggestSomething = []Suggestion{{Name: ""}}
+	SuggestNothing   []Suggestion
+	SuggestFlag      = []Suggestion{{Name: "--"}}
+)
 
 type Suggestion struct {
 	Name string
 	Desc string
 }
 
-// e.g. AutoComplete("git sta", completionTree, strings.Prefix)
-func AutoComplete(text string, completionTree *CompTree, queryFunc SearchMethod) ([]Suggestion, error) {
-	var prev *CompTree = nil
-	curr := completionTree
-	curr = prev
-	curr = completionTree
+//
+//monk > cluster|user|load etc
+//monk - > -- >  flags
+//monk -- > flags
+//monk cluster > info|peers etc
+//monk cluster peers > --nocolor etc
+//monk run > list of runnables
+//
+//
+// Псевдокод
+//* prev:
+//	* флаг
+//		* есть predictor -> execute и return
+//		* takes values -> return nothing
+//		* next
+//	* команда
+//		* search subcommands
+//		* predictor?
+//		* -- > флаги
+//* - or --  - флаги
+//* flag + = || flag + " " - > flag value
+//* subcommand
 
+//
+// задачи:
+//выполнять динамик функции
+//считать - так же как и --
+
+func AutoComplete(text string, completionTree *CompTree, queryFunc SearchMethod) ([]Suggestion, error) {
 	fields, err := shlex.Split(strings.TrimSpace(text))
 	if err != nil {
 		return nil, err
 	}
+	suggestions := autoComplete(completionTree, fields, text, queryFunc)
+	return suggestions, nil
+}
+
+func autoComplete(completionTree *CompTree, fields []string, text string, queryFunc SearchMethod) []Suggestion {
+	// current (last) command (not flag)
+	curr := completionTree
+	// global flags
+	globalFlags := completionTree.Flags
+
 	last := fields[len(fields)-1]
 	// i.e. in "git checkout --fie", remove "git"
 	fields = fields[1:]
 	for i, field := range fields {
-		//if strings.HasPrefix(field, "-") {
-		//	continue
-		//}
+		if strings.HasPrefix(field, "-") {
+			continue
+		}
 		if i == len(fields)-1 && !strings.HasSuffix(text, " ") {
 			continue
 		}
 		if curr.Sub[field] != nil {
-			prev = curr
 			curr = curr.Sub[field]
-		} else if curr.Args[field] != nil {
-			prev = curr
-			curr = curr.Args[field]
 		}
 	}
+	// get suggestions
 	s := make([]Suggestion, 0, 20)
-	// ends with sub & space
-	if curr == nil {
-		return s, nil
-	} else if strings.HasSuffix(text, " ") {
-		if curr.Args != nil {
-			for k, v := range curr.Args {
-				s = append(s, Suggestion{
-					Name: k,
-					Desc: v.Desc,
-				})
-			}
-		}
-		if curr.Sub != nil {
-			for k, v := range curr.Sub {
-				s = append(s, Suggestion{
-					Name: k,
-					Desc: v.Desc,
-				})
-			}
-		}
-		if curr.Dynamic != nil {
-			s = append(s, curr.Dynamic("")...)
-		}
+	name := last
+	switch {
+	case curr == nil:
+		return s
+	case strings.HasSuffix(text, " "):
+		// complete command argument | flag value | subcommands | flags
+		return completeSpace(curr, globalFlags, name)
+	case strings.HasPrefix(last, "-") && strings.HasSuffix(last, "="):
+		// complete flag value
+		return completeFlagValue(curr, globalFlags, name)
+	case strings.HasPrefix(last, "-"):
+		// complete flag
+		return completeFlags(curr, globalFlags, name, fields)
+	default:
+		// complete partial ("search") argument
+		return searchArgs(curr, queryFunc, last)
+	}
+}
 
-	} else if strings.HasPrefix(last, "-") { // ends with flag
-		if last == "-" {
-			last = "--"
+// search for args or subcommand
+func searchArgs(curr *CompTree, searchFunc SearchMethod, segment string) []Suggestion {
+	var resp []Suggestion
+	// command argument
+	if curr.TakesValue {
+		if curr.Dynamic != nil {
+			return curr.Dynamic(segment)
 		}
-		hasTwoDashes := strings.HasPrefix(last, "--")
-		searchTerm := strings.TrimPrefix(last, "-")
-		searchTerm = strings.TrimPrefix(searchTerm, "-")
-		for k, v := range curr.Flags {
-			if hasTwoDashes && len(k) > 1 && (strings.HasPrefix(k, searchTerm) || searchTerm == "") {
-				s = append(s, Suggestion{
-					Name: k,
-					Desc: v.Desc,
-				})
+		return SuggestSomething
+	}
+	// subcommands
+	for k, v := range curr.Sub {
+		if searchFunc(k, segment) {
+			resp = append(resp, Suggestion{
+				Name: k,
+				Desc: v.Desc,
+			})
+		}
+	}
+	return resp
+}
+func searchFlags(flags map[string]*CompTree, searchTerm string, fields []string) []Suggestion {
+	var resp []Suggestion
+	for k, v := range flags {
+		if strings.HasPrefix(k, searchTerm) || searchTerm == "" {
+			if searchTerm != "" && containsFlag(fields, k) { // don't suggest flag which is already set
+				continue
 			}
-			if !hasTwoDashes && len(k) == 1 {
-				s = append(s, Suggestion{
-					Name: k,
-					Desc: v.Desc,
-				})
+			resp = append(resp, Suggestion{
+				Name: k,
+				Desc: v.Desc,
+			})
+		}
+	}
+	return resp
+}
+
+func containsFlag(arr []string, x string) bool {
+	if contains(arr, "--"+x) {
+		return true
+	}
+	return contains(arr, "-"+x)
+}
+
+func completeFlagValue(curr *CompTree, globalFlags map[string]*CompTree, name string) []Suggestion {
+	name = strings.Trim(name, "-=")
+
+	// if command flag expects a value
+	if flag := curr.Flags[name]; flag != nil {
+		// flag has custom suggestion method
+		if flag.Dynamic != nil {
+			return flag.Dynamic("")
+		}
+		// do not suggest anything, wait for user to input value
+		if flag.TakesValue {
+			return SuggestSomething
+		}
+	}
+	// if global flag expects a value
+	if flag := globalFlags[name]; flag != nil {
+		// flag has custom suggestion method
+		if flag.Dynamic != nil {
+			return flag.Dynamic("")
+		}
+		// do not suggest anything, wait for user to input value
+		if flag.TakesValue {
+			return SuggestSomething
+		}
+	}
+	return SuggestNothing
+}
+
+func completeFlags(curr *CompTree, globalFlags map[string]*CompTree, name string, fields []string) []Suggestion {
+	searchTerm := strings.TrimLeft(name, "-")
+	// complete flag of current command
+	if s := searchFlags(curr.Flags, searchTerm, fields); len(s) > 0 {
+		return s
+	}
+	if s := searchFlags(globalFlags, searchTerm, fields); len(s) > 0 {
+		return s
+	}
+	return SuggestNothing
+}
+
+// TODO: think about suggesting multiple args?
+// TODO: fix suggesting flags after command arguments
+func completeSpace(curr *CompTree, globalFlags map[string]*CompTree, name string) []Suggestion {
+	isFlag := strings.HasPrefix(name, "-")
+	isCurrent := curr.Name == name // is last arg is current command name
+
+	name = strings.Trim(name, "- ")
+
+	if isFlag {
+		// if command flag expects a value
+		if flag := curr.Flags[name]; flag != nil {
+			// flag has custom suggestion method
+			if flag.Dynamic != nil {
+				return flag.Dynamic("")
+			}
+			// do not suggest anything, wait for user to input value
+			if flag.TakesValue {
+				return SuggestSomething
+			}
+		}
+		// if global flag expects a value
+		if flag := globalFlags[name]; flag != nil {
+			// flag has custom suggestion method
+			if flag.Dynamic != nil {
+				return flag.Dynamic("")
+			}
+			// do not suggest anything, wait for user to input value
+			if flag.TakesValue {
+				return SuggestSomething
 			}
 		}
 	}
-	// if NOT a flag and IS a "search" value
-	if !strings.HasPrefix(last, "-") && !strings.HasSuffix(text, " ") {
-		for k, v := range curr.Args {
-			if queryFunc(k, last) {
-				s = append(s, Suggestion{
-					Name: k,
-					Desc: v.Desc,
-				})
-			}
-		}
+
+	// if current command has custom suggestion method and
+	// last arg was its name or flag which don't expect argument
+	if curr.Dynamic != nil && (isCurrent || isFlag) {
+		return curr.Dynamic("")
+	}
+
+	// suggest subcommand
+	if curr.Sub != nil {
+		var resp []Suggestion
 		for k, v := range curr.Sub {
-			if queryFunc(k, last) {
-				s = append(s, Suggestion{
-					Name: k,
-					Desc: v.Desc,
-				})
-			}
-		}
-		if curr.Dynamic != nil {
-			for _, v := range curr.Dynamic("") {
-				if queryFunc(v.Name, last) {
-					s = append(s, Suggestion{
-						Name: v.Name,
-						Desc: v.Desc,
-					})
-				}
-			}
-		}
-	}
-	// ends with = (suggest flag args)
-	if strings.HasPrefix(last, "-") && strings.HasSuffix(last, "=") {
-		flagName := strings.TrimPrefix(last, "-")
-		flagName = strings.TrimPrefix(flagName, "-")
-		flagName = strings.TrimSuffix(flagName, "=")
-		flagCompleter := curr.Flags[flagName]
-		for k, v := range flagCompleter.Args {
-			s = append(s, Suggestion{
+			resp = append(resp, Suggestion{
 				Name: k,
 				Desc: v.Desc,
 			})
 		}
-		for k, v := range flagCompleter.Sub {
-			s = append(s, Suggestion{
-				Name: k,
-				Desc: v.Desc,
-			})
-		}
+		return resp
 	}
 
-	// ends with flag & space
-	// ends with sub
-
-	return s, nil
+	// suggest flags (no subcommands or values is expected so suggest only thing that is available: flags
+	if isCurrent && (len(curr.Flags) > 0 || len(globalFlags) > 0) {
+		return SuggestFlag
+	}
+	// there are no flags or last arg is command argument so we can suggest nothing
+	// TODO: case when it's after FLAG argument not command
+	return SuggestNothing
 }
